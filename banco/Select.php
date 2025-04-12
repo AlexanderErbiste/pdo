95a93d346e2c40b1d22df1c33b37f5e74d9f2cf2
Alexander Erbiste <167037848+AlexanderErbiste@users.noreply.github.com>
2025-04-12 00:37:11 -0300

    Add files via upload

--- banco/Select.php
+++ banco/Select.php
@@ -1,17 +1,18 @@
 <?php
-    
+
     include('database.php');
 
-    //executar comandos sql(select)
+    // Executar comando sql(select):
+        
+    $sql = "SELECT * FROM TB_USUARIO";
 
-    $sql = " SELECT * FROM TB_USUARIO " ;
-
-   //$result = $con->query($sql);
-
-    foreach( $con->query($sql) as $value ){
-        echo $value[0] ." ". $value[1] . $value[2] . "<br>";
-  
+    // $result = $con->query($sql);
+    foreach($con->query($sql) as $value)
+    {
+        echo $value[0] . " " . $value[1] . " " . $value[2] . " " . "<br>";
     }
+    
+    
 
-    echo "ola mundo"
+
 ?>
--- banco/database.php
+++ banco/database.php
@@ -1,17 +1,21 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "projetopdo";
 
-try{
-$con = new PDO("mysql:host=$host;dbname=$database",$username, $password);
-$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "projetopdo";
 
+    try
+    {
 
-echo "Conectado";
-}
-catch(PDOException $e){
-    echo "FALHA". $e->getMessage();
-}
+        $con = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conectado" . "<br>";
+
+    }
+    catch(PDOException $e)
+    {
+        echo "Falha" . $e->getMessage();
+    }
+
 ?>
